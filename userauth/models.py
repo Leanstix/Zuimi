52ca5873aa6d432ab663f907c2059a3a97fd6500
Leanstix <leanstixx@gmail.com>
2025-02-19 18:59:21 +0100

    started user authentication

--- userauth/models.py
+++ userauth/models.py
@@ -57,3 +57,34 @@ class CustomUserManager(BaseUserManager):
         except Exception as e:
             logger.error(f"Error during user registration: {e}")
             raise ValidationError("Error during user registration")
+        
+
+class User(AbstractBaseUser, PermissionsMixin):
+    GENDER_CHOICES = [('M', 'Male'), ('F', 'Female'), ('O', 'Other')]
+    email = models.EmailField(unique=True, validators=[EmailValidator()])
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    activation_token = models.CharField(max_length=100, default=get_random_string)
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return super().__str__()
+    
+    def save(self, *args, **kwargs):
+        if not self.user_name and not self.activation_token and not self.email_verified:
+            self.user_name = self.email
+        super().save(*args, **kwargs)
+
+    def activate(self, token):
+        if token == self.activation_token:
+            self.is_active = True
+            self.email_verified = True
+            self.activation_token = None
+            self.save(update_fields=['is_active', 'email_verified', 'activation_token'])
+            return True
+        return False
