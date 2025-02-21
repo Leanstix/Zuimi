4caff9d9ec9d21546b0c3c602fa6947ed91a08da
Leanstix <leanstixx@gmail.com>
2025-02-21 20:23:18 +0100

    user authentication view

--- userauth/views.py
+++ userauth/views.py
@@ -2,9 +2,51 @@ import os
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserRegistrationSerializer, UserActivationSerializer, UserProfileUpdateSerializer, ChangePasswordSerializer
+from .serializers import UserRegistrationSerializer, UserActivationSerializer, ChangePasswordSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import UpdateAPIView, AllowAny
 from django.contrib.auth import get_user_model
 from .models import User
 import logging
+
+logger = logging.getLogger(__name__)
+User = get_user_model()
+
+class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Account created successfully! Please check your email to activate your account."}, status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserActivationView(APIView):
+    permission_classes = [AllowAny] 
+
+    def post(self, request):
+        serializer = UserActivationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Account activated successfully!"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ActivateAccountView(APIView):
+    def get(self, request):
+        try:
+            user = User.objects.get(activation_token=request.GET.get('token'))
+            user.activate_account()
+            return Response({"message": "Account activated successfully!"}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"message": "Invalid or expired activation token."}, status=status.HTTP_400_BAD_REQUEST)
+        
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        serializer = ChangePasswordSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Password changed successfully!"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
