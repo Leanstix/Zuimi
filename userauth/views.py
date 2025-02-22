f7de1ed9a1f02cceff7dd1176746f9f95f27edc6
Leanstix <leanstixx@gmail.com>
2025-02-22 02:52:01 +0100

    added user profile update

--- userauth/views.py
+++ userauth/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserRegistrationSerializer, UserActivationSerializer, ChangePasswordSerializer
+from .serializers import UserRegistrationSerializer, UserActivationSerializer, ChangePasswordSerializer, UpdateUserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import get_user_model
 
@@ -26,6 +26,20 @@ class UserActivationView(APIView):
             serializer.save()
             return Response({"message": "Account activated successfully!"}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserProfileUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        serializer = UpdateUserSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        serializer = UpdateUserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class ActivateAccountView(APIView):
     permission_classes = [AllowAny]
