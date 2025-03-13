2c77657cad91ae69441415859fee5c600879a581
Leanstix <leanstixx@gmail.com>
2025-03-13 09:23:24 +0100

    tweaking the user model

--- userauth/models.py
+++ userauth/models.py
@@ -46,7 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=100, blank=True, null=True)
     last_name = models.CharField(max_length=100, blank=True, null=True)
     user_name = models.CharField(max_length=100, unique=True, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True, validators=[RegexValidator(r'^\+?1?\d{9,15}$')], blank=True, null=True, unique=True)
+    phone_number = models.CharField(max_length=15, validators=[RegexValidator(r'^\+?1?\d{9,15}$')], blank=True, null=True, unique=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
